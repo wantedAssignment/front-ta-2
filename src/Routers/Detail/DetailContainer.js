2665d90aa084ac344523ee9e1221f8b29a9f11dd
woogi337 <jiwook131@gmail.com>
2021-07-30 16:59:18 +0900

    feat: Detail UI 추가

--- src/Routers/Detail/DetailContainer.js
+++ src/Routers/Detail/DetailContainer.js
@@ -1,14 +1,66 @@
 import React from 'react';
+// import backendAPI from '../../api';
 import DetailPresenter from './DetailPresenter';
 
-class DetailContainer extends React.Component {
-  state = {};
+const ITEM = ['제목', 'Brand', '가격'];
 
-  componentDidMount() {}
+class DetailContainer extends React.Component {
+  state = {
+    dataArr: [],
+    data: {},
+    id: Number(this.props.location.pathname.split('/').slice(-1)),
+  };
+
+  readRandom = () => {
+    // const MAX = 50;
+    // let newID = Math.floor(Math.random() * MAX) + 1;
+    // while ( newID === this.state.id) {
+    //   newID = Math.floor(Math.random() * MAX) + 1;
+    // };
+    // this.setState({
+    //   id: newID,
+    // });
+  };
+  
+  setDislike = () => {
+    // this.readRandom();
+  }
+
+  read = async () => {
+    // try {
+    //   const result = await backendAPI.loadData();
+    //   this.setState({
+    //     dataArr: result,
+    //   });
+    //   const data = result.filter(v => v.id === this.state.id);
+    //   this.setState({
+    //     data: data[0],
+    //   });
+    // } catch (error) {
+    //   alert('오류 발생');
+    // };
+  };
+
+  componentDidMount() {
+    // this.read();
+  };
+
+  componentDidUpdate(_, prev) {
+    // if (this.state.id !== prev.id) {
+    //   this.read();
+    // };
+  };
 
   render() {
-    return <DetailPresenter />;
-  }
-}
+    return (
+      <DetailPresenter
+        data={this.state.data}
+        ITEM={ITEM}
+        readRandom={this.readRandom}
+        setDislike={this.setDislike}
+      />
+    );
+  };
+};
 
 export default DetailContainer;
--- src/Routers/Detail/DetailPresenter.js
+++ src/Routers/Detail/DetailPresenter.js
@@ -1,7 +1,91 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const DetailPresenter = (props) => {
-  return <div>상세화면입니다</div>;
+const HomePresenter = ({ data, ITEM, readRandom, setDislike }) => {
+  return (
+    <Wrapper>
+      <Container>
+        <ButtonWrapper>
+          <Button onClick={readRandom}>랜덤조회</Button>
+          <Button onClick={setDislike}>관심없음</Button>
+        </ButtonWrapper>
+        <ItemWrapper>
+          {ITEM.map((v, i) =>(
+            <ItemSubWrapper key={i}>
+              <Item>{v} :</Item>
+              {/* <Value>{i === 0 && data?.title}</Value> */}
+              {/* <Value>{i === 1 && data?.brand}</Value> */}
+              {/* <Value>{i === 2 && `${data?.price} 원`}</Value> */}
+            </ItemSubWrapper>
+          ))}
+        </ItemWrapper>
+      </Container>
+    </Wrapper>
+  );
 };
 
-export default DetailPresenter;
+export default HomePresenter;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 100px;
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 10px 10px 10px rgba(0,0,0,0.3);
+  width: 800px;
+  border: 1px solid rgba(0,0,0,0.1);
+  border-radius: 15px;
+  background-color: #FADCD9;
+`;
+const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 20px;
+`;
+const Button = styled.button`
+  border: none;
+  color: white;
+  background-color: #F8AFA6;
+  border-radius: 10px;
+  width: 60px;
+  height: 30px;
+  margin-right: 10px;
+  font-size: 11px;
+  box-shadow: 3px 3px 2px rgba(0,0,0,0.3);
+  cursor: pointer;
+`;
+const ItemWrapper = styled.div`
+  width: 100%;
+  border-top: 1px solid rgba(0,0,0,0.1);
+  padding-left: 40px;
+  padding-top: 30px;
+  background-color: #F9F1F0;
+  border-bottom-right-radius: 15px;
+  border-bottom-left-radius: 15px;
+  font-size: 20px;
+  
+  section:first-child {
+    font-size: 32px;
+  }
+`;
+const ItemSubWrapper = styled.section`
+  margin-bottom: 45px;
+  display: flex;
+  align-items: center;
+`;
+const Item = styled.div`
+  margin-right: 15px;
+  font-weight: bold;
+  color: rgba(0,0,0,0.6);
+  width: 100px;
+  text-align: end;
+`;
+const Value = styled.span`
+  color: rgba(0,0,0,0.8);
+  font-size: 18px;
+`;
