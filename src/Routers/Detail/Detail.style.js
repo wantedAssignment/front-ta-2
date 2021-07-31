c7083eabd66eae87d5c31ddbcf8fff2a5192af1f
yujin mac <kimyu28jin@gmail.com>
2021-07-31 19:50:08 +0900

    Design : Detail 스타일 수정

--- src/Routers/Detail/Detail.style.js
+++ src/Routers/Detail/Detail.style.js
@@ -3,34 +3,33 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-  padding-top: 100px;
+  align-items: center;
 `;
 
 export const Container = styled.div`
+  margin-top: 40px;
+  padding: 20px;
+  min-width: 700px;
+  background-color: #fadcd9;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  box-shadow: 4px 4px 10px rgba(37, 13, 13, 0.15);
-  width: 700px;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  border-radius: 15px;
-  background-color: #fadcd9;
+  border-radius: 10px;
 `;
 
 export const ButtonWrapper = styled.div`
-  width: 100%;
   display: flex;
-  padding: 20px;
+  justify-content: space-between;
+  width: 100%;
 `;
 
 export const Button = styled.button`
   border: none;
   color: white;
   background-color: #f8afa6;
-  border-radius: 15px;
+  border-radius: 10px;
   padding: 10px 20px;
-  margin-right: 10px;
   font-size: 13px;
   cursor: pointer;
   transition: 0.5s;
@@ -38,7 +37,6 @@ export const Button = styled.button`
   &:hover {
     background-color: #f39f94;
   }
-
   &:last-of-type {
     background-color: #c6c6c6;
     &:hover {
@@ -47,35 +45,18 @@ export const Button = styled.button`
   }
 `;
 export const ItemWrapper = styled.div`
-  width: 100%;
-  border-top: 1px solid rgba(0, 0, 0, 0.1);
-  padding-left: 40px;
-  padding-top: 30px;
-  background-color: #f9f1f0;
-  border-bottom-right-radius: 15px;
-  border-bottom-left-radius: 15px;
   font-size: 20px;
-
-  /* section:first-child {
-    font-size: 32px;
-  } */
+  padding: 40px 0;
 `;
 
-export const ItemSubWrapper = styled.section`
-  margin-bottom: 45px;
-  display: flex;
-  align-items: center;
+export const ItemSubWrapper = styled.div`
+  margin-bottom: 20px;
 `;
 
-export const Item = styled.div`
-  margin-right: 15px;
-  font-weight: bold;
-  color: rgba(0, 0, 0, 0.6);
-  text-align: end;
+export const Item = styled.h2`
+  display: inline-block;
   font-size: 24px;
+  font-weight: 600;
 `;
 
-export const Value = styled.span`
-  color: rgba(0, 0, 0, 0.8);
-  font-size: 18px;
-`;
+export const Value = styled.span``;
