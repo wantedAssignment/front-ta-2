ca5f503f506cd8070d63d148685961a9941b1ba4
yujin mac <kimyu28jin@gmail.com>
2021-07-31 18:33:15 +0900

    Feat : 상세 이동 기능 추가

--- src/Routers/Product/ProductPresenter.js
+++ src/Routers/Product/ProductPresenter.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const ProductPresenter = ({ data, handleAddItem }) => {
   return (
@@ -9,12 +11,14 @@ const ProductPresenter = ({ data, handleAddItem }) => {
           onClick={() => handleAddItem(item)}
           style={{ margin: 30, cursor: 'pointer' }}
         >
-          <span>{item.id}</span>
-          <p>{item.title}</p>
-          <span>{item.brand}</span>
-          <span>{item.price}</span>
-          <span>{item.date}</span>
-          <span>{String(item.liked)}</span>
+          <Link to={`/product/${item.id}`}>
+            <span>{item.id}</span>
+            <p>{item.title}</p>
+            <span>{item.brand}</span>
+            <span>{item.price}</span>
+            <span>{item.date}</span>
+            <span>{String(item.liked)}</span>
+          </Link>
         </li>
       ))}
     </ul>
