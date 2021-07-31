d71b9b79a30bd0c0ecf076a43e11ad538da69834
yujin mac <kimyu28jin@gmail.com>
2021-07-31 19:28:14 +0900

    Design: Header 스타일 수정

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Navigation = withRouter(({ location: { pathname } }) => {
   return (
-    <>
+    <Header>
       <Link to="/product">Product</Link>
       <Link to="/recentList">RecentList</Link>
-    </>
+    </Header>
   );
 });
 
 export default Navigation;
+
+const Header = styled.header`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  background-color: #ececec;
+  margin-bottom: 30px;
+
+  > a {
+    display: inline-block;
+    padding: 0 20px;
+    font-size: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &:before {
+      content: '';
+      position: absolute;
+      bottom: 0px;
+      left: 0;
+      width: 100%;
+      height: 2px;
+      background-color: #000;
+      opacity: 0;
+      transition: 0.3s;
+    }
+
+    &:hover {
+      &:before {
+        opacity: 1;
+      }
+    }
+  }
+`;
